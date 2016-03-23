1c5480a5d9fc6a244c5630f9dc320a946e595739
ernesto <emello969@gmail.com>
2016-03-22 23:25:59 -0300

    primer commit

---
+++ prueba/principal.cpp
@@ -0,0 +1,20 @@
+/*
+ * principal.cpp
+ *
+ *  Created on: 18 de mar. de 2016
+ *      Author: ernesto
+ */
+
+#include<iostream>
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	std::cout <<"hola mundo";
+	return 0;
+}
+
+
